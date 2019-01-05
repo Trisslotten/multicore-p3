75f14ea2ff4beffbf51baab3d7914260e98e73a7
Trisslotten <tristan23@live.se>
2019-01-06 00:01:56 +0100

    changed to 2 pass 1D kernel instead

--- multicore-p3/main.cu
+++ multicore-p3/main.cu
@@ -23,7 +23,7 @@ __global__ void unweaveKernel(ColorType* src, ColorType* dst, int numPixels)
 	}
 }
 
-__global__ void blurKernel(ColorType* src, ColorType* blurred, int width, int height, float* mask)
+__global__ void blurKernel(ColorType* src, ColorType* blurred, int width, int height, float* mask, bool vertPass)
 {
 	int ix = threadIdx.x + blockIdx.x * blockDim.x;
 	int iy = threadIdx.y + blockIdx.y * blockDim.y;
@@ -36,22 +36,28 @@ __global__ void blurKernel(ColorType* src, ColorType* blurred, int width, int he
 		float sumG = 0.f;
 		float sumB = 0.f;
 
-		for (int y = 0; y < 5; y++)
+		for (int m = 0; m < 5; m++)
 		{
-			for (int x = 0; x < 5; x++)
+			int cx = ix;
+			int cy = iy;
+			if (vertPass)
 			{
-				int cx = ix + x - 2;
-				int cy = iy + y - 2;
 				// clamp to edge
-				cx = max(0, min(width - 1, cx));
-				cy = max(0, min(height - 1, cy));
-				int i = cx + cy * width;
-
-				float currMask = mask[x + y * 5];
-				sumR += currMask * src[i];
-				sumG += currMask * src[i+stride];
-				sumB += currMask * src[i+2*stride];
+				cy += m - 2;
+				cy = max(0, min(width - 1, cy));
 			}
+			else
+			{
+				cx += m - 2;
+				cx = max(0, min(width - 1, cx));
+			}
+			
+			int i = cx + cy * width;
+
+			float currMask = mask[m];
+			sumR += currMask * src[i];
+			sumG += currMask * src[i+stride];
+			sumB += currMask * src[i+2*stride];
 		}
 
 		int i = ix + iy * width;
@@ -65,14 +71,15 @@ __global__ void blurKernel(ColorType* src, ColorType* blurred, int width, int he
 int main()
 {
 	Timer t;
-
-	CImg<ColorType> image("cake.ppm");
+	
+	const char* file = "cake.ppm";
+	CImg<ColorType> image(file);
 	int width = image.width();
 	int height = image.height();
 
 	image.permute_axes("cxyz");
 	
-	
+	/*
 	CImg<float> mask5(5, 5);
 	mask5(0, 0) = mask5(0, 4) = mask5(4, 0) = mask5(4, 4) = 1.f / 256.f;
 	mask5(0, 1) = mask5(0, 3) = mask5(1, 0) = mask5(1, 4) = mask5(3, 0) = mask5(3, 4) = mask5(4, 1) = mask5(4, 3) = 4.f / 256.f;
@@ -80,10 +87,16 @@ int main()
 	mask5(1, 1) = mask5(1, 3) = mask5(3, 1) = mask5(3, 3) = 16.f / 256.f;
 	mask5(1, 2) = mask5(2, 1) = mask5(2, 3) = mask5(3, 2) = 24.f / 256.f;
 	mask5(2, 2) = 36.f / 256.f;
+	*/
+
+	float mask[5];
+	mask[0] = mask[4] = 1.f / 16.f;
+	mask[1] = mask[3] = 4.f / 16.f;
+	mask[2] = 6.f / 16.f;
 
 
 	size_t imgMemSize = sizeof(ColorType)*image.size();
-	size_t maskMemSize = sizeof(float) * 5 * 5;
+	size_t maskMemSize = sizeof(float) * 5;
 
 	ColorType* d_interleavedImg = nullptr;
 	ColorType* d_img = nullptr;
@@ -94,15 +107,15 @@ int main()
 	cudaMalloc((void**)&d_blurImg, imgMemSize);
 	cudaMalloc((void**)&d_mask, maskMemSize);
 	cudaMemcpy(d_interleavedImg, image.data(), imgMemSize, cudaMemcpyHostToDevice);
-	cudaMemcpy(d_mask, mask5.data(), maskMemSize, cudaMemcpyHostToDevice);
+	cudaMemcpy(d_mask, mask, maskMemSize, cudaMemcpyHostToDevice);
 
 
 	int size = image.size() / 3;
 	int unweaveBlockSize = 128;
 	int unweaveNumBlocks = std::ceil(double(size) / unweaveBlockSize);
-	unweaveKernel<<<unweaveNumBlocks, unweaveBlockSize >>>(d_interleavedImg, d_img, size);
+	unweaveKernel<<<unweaveNumBlocks, unweaveBlockSize>>>(d_interleavedImg, d_img, size);
 
-	dim3 blurBlockSize(17, 17);
+	dim3 blurBlockSize(32, 32);
 	dim3 blurNumBlocks(std::ceil(double(width) / blurBlockSize.x),
 					   std::ceil(double(height) / blurBlockSize.y));
 
@@ -112,21 +125,24 @@ int main()
 
 	cudaDeviceSynchronize();
 	t.restart();
-	blurKernel<<<blurNumBlocks, blurBlockSize>>>(d_img, d_blurImg, width, height, d_mask);
+	blurKernel<<<blurNumBlocks, blurBlockSize>>>(d_img, d_blurImg, width, height, d_mask, true);
+	std::swap(d_img, d_blurImg);
+	blurKernel<<<blurNumBlocks, blurBlockSize>>>(d_img, d_blurImg, width, height, d_mask, false);
 	cudaDeviceSynchronize();
 	double elapsed = t.elapsed();
 	std::cout << "elapsed: " << elapsed << "\n";
 
 	cudaMemcpy(image.data(), d_blurImg, imgMemSize, cudaMemcpyDeviceToHost);
 
-	for (int i = 0; i < 3; i++)
-	{
-		std::cout << (int)image.data()[i] << " " << (int)image.data()[i+width*height] << " " << (int)image.data()[i + 2*width * height] << "\n";
-	}
+	CImg<ColorType> original(file);
 
-	CImgDisplay display(image, "bogdan");
+	CImgDisplay display1(original, "original");
+	CImgDisplay display2(image, "blurred");
 	while (true)
-		display.wait();
+	{
+		display1.wait();
+		display2.wait();
+	}
 
 	return 0;
 }
